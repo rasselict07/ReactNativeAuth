adbecda17ea8685a8179683f1be8b54a1f6c57c0
rasselict07 <rezaul07012>
2018-12-29 00:51:42 +0600

    install firebase and add config to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './component/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDAMHbYeGDFDAsuBhCjcrFnjP_ThsC_zow',
+            authDomain: 'reactnativeauth-ab25b.firebaseapp.com',
+            databaseURL: 'https://reactnativeauth-ab25b.firebaseio.com',
+            projectId: 'reactnativeauth-ab25b',
+            storageBucket: 'reactnativeauth-ab25b.appspot.com',
+            messagingSenderId: '877365345498'
+        });
+    }
+
     render() {
         return (
             <View>
